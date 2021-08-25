0e52d8d7bde2b290cb1cdc960d1d38ddcf56f47a
Volodymyr Moruhin <vova.morugin@gmail.com>
2021-08-24 17:29:39 -0700

    changes in userZipCodes component

--- src/Views/UserZipCodes.js
+++ src/Views/UserZipCodes.js
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from 'react'
-import { ListGroup, Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import { useAuth } from "../Contexts/AuthContext"
 import ZipCodeContext from '../Contexts/zipCode'
 
@@ -10,7 +10,7 @@ export default function UserZipCodes() {
 
     useEffect(() => {
         for (const id in userZipCodes) {
-            if (userZipCodes[id] === selectedZipcode) {
+            if (userZipCodes[id] == selectedZipcode) {
                 setAlreadySaved(true)
                 break
             }
@@ -19,29 +19,47 @@ export default function UserZipCodes() {
     }, [userZipCodes, selectedZipcode])
 
 
-    const handleOnClick = () => {
+    const handleSave = () => {
         addZipCode(selectedZipcode)
     }
 
-    const handleDelete = (key) => {
-        deleteZipCode(key)
+    const handleDelete = () => {
+        var zipCodeId = null
+        for (const key in userZipCodes) {
+            console.log(userZipCodes[key])
+            if (userZipCodes[key] == selectedZipcode) {
+                zipCodeId = key
+            }
+        }
+        if (zipCodeId !== null) {
+            deleteZipCode(zipCodeId)
+        }
+    }
+
+    const handleLoad = (zipCode) => {
+        setZipcode(zipCode)
     }
     return (
         <div>
-            {currentUser != null && selectedZipcode != null &&
-                <Button variant="primary" disabled={alreadySaved} onClick={handleOnClick}>Save selected zipcode ({selectedZipcode})</Button>}
+            {currentUser != null && selectedZipcode != null && !alreadySaved &&
+                <Button variant="primary" onClick={handleSave}>Add to favourites ({selectedZipcode})</Button>}
+
+            {currentUser != null && selectedZipcode != null && alreadySaved &&
+                <Button variant="danger" onClick={handleDelete}>Remove from favourites ({selectedZipcode})</Button>}
+
             {userZipCodes != null && Object.keys(userZipCodes).length > 0 ?
-                <div> Saved Zip codes:
-                    <ListGroup>
-                        {Object.keys(userZipCodes).map(function (key, index) {
-                            return <ListGroup.Item key={index}>
-                                <Button style={{ 'width': '90%' }} variant="light" size="sm" onClick={() => setZipcode(userZipCodes[key])}>{userZipCodes[key]}</Button>
-                                <Button style={{ 'width': '10%' }} className="float-end" size="sm" variant="outline-danger" onClick={() => handleDelete(key)}>x</Button>
-                            </ListGroup.Item>
 
-                        })}
-
-                    </ListGroup>
+                <div>
+                    Favorite zip codes:
+                         <select className="ui fluid search dropdown"
+                        onInput={(e) => handleLoad(e.target.value)}
+                    >
+                        {Object.keys(userZipCodes).map((key, index) =>
+                            <option key={index} zipcodeid={key}>
+                                {userZipCodes[key]}
+                            </option>
+                        )}
+                    </select>
                 </div> : null}
         </div>
 
