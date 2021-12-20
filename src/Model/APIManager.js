13a9be20a6d587ee2c6689ed36578b65787e8c32
Volodymyr Moruhin <vova.morugin@gmail.com>
2021-12-20 10:47:45 -0800

    Update APIManager.js

--- src/Model/APIManager.js
+++ src/Model/APIManager.js
@@ -7,7 +7,7 @@ const defaultParams = {
     f: 'json'
 }
 
-const startingMonth = '8'
+const startingMonth = '10'
 const API_URL = 'https://gis-public.sandiegocounty.gov/arcgis/rest/services/Hosted/COVID19_CaseRateData/FeatureServer/1/query'
 
 
